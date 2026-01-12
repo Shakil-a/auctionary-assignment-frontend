181e324d50b2cdbcfdb7d069aeefed25961b9a85
shakil ahmed <ahmed1shakil@gmail.com>
2026-01-12 11:19:13 +0000

    Added auth guard to routes

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -5,11 +5,20 @@ import Login from "../views/pages/Login.vue";
 import Register from "../views/pages/Register.vue"
 import SingleItem from "../views/pages/SingleItem.vue"
 
+const ifAuthenticated = (to, from, next) => {
+    const loggedIn = localStorage.getItem('session_token');
+    if (loggedIn) {
+        next()
+        return
+    }
+    next('/login')
+}
+
 const routes = [
     { path: "/", component: Home},
     { path: "/login", component: Login},
     { path: "/register", component: Register},
-    { path: "/item/:id", component: SingleItem}
+    { path: "/item/:id", component: SingleItem, beforeEnter: ifAuthenticated}
 ]
 
 const router = createRouter({
